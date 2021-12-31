4d37132d69fec4ca0a5c128fb8df739ac868d061
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-31 16:43:11 -0300

    Add repr method to Board class

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ class Board:
     self.cols = columns
     self.board = [ [" "] * self.cols for _ in range(self.rows) ]
 
+  def __repr__(self):
+
+    header = "  " + "   ".join(list(map(str, range(1, self.cols + 1)))) + "  \n"
+
+    separator = "---".join(["+"] * (self.cols + 1)) + "\n"
+
+    out = header + separator
+    for row in self.board:
+      for cell in row:
+        out += f"| {cell} "
+      out += "|\n" + separator
+    return out
+
 board = Board(7, 6)
-print(board.board)
 print(board)
