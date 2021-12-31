e16c6043e36fe0e39333f128e35545bfbe669680
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-31 16:24:55 -0300

    Add board size as init parameters

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class Board:
 
-  def __init__(self):
-    self.board = [ [" "] * 7 for _ in range(6) ]
+  def __init__(self, columns, rows):
+    self.rows = rows
+    self.cols = columns
+    self.board = [ [" "] * self.cols for _ in range(self.rows) ]
 
-board = Board()
+board = Board(7, 6)
 print(board.board)
 print(board)
